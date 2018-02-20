01825fe735438bc5c928938223ee7ca42f8f45e6
agungpradanaw <33536926+agungpradanaw@users.noreply.github.com>
2018-02-20 15:53:45 +0700

    login

---
+++ application/views/login/header.php
@@ -0,0 +1,20 @@
+<link href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css')?>" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css')?>">
+
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!-- Site Properties -->
+    <title>Bootstrap 4 Login Form</title>
+
+    <!-- Stylesheets -->
+
+</head>
+<body>
