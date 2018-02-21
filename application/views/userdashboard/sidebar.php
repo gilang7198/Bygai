cb91f88555e1c84d3fe37c38775a1d5942cd7781
agungpradanaw <33536926+agungpradanaw@users.noreply.github.com>
2018-02-21 14:49:48 +0700

    lamputengah.php

--- application/views/userdashboard/sidebar.php
+++ application/views/userdashboard/sidebar.php
@@ -4,17 +4,8 @@
       <!-- Sidebar user panel -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li><a href="#"><i data-feather="home"></i> <span>Dashboard</span></a></li>
+        <li><a href="#"><i class="fa fa-pie-chart"></i> <span>Dashboard</span></a></li>
         <li class="header">MAIN NAVIGATION</li>
-        <li><a href="#"><i class="fa fa-dashboard"></i> <span>Device Option</span></a></li>
-        <li>
-          <a href="pages/widgets.html">
-            <i class="fa fa-th"></i> <span>Widgets</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-green">new</small>
-            </span>
-          </a>
-        </li>
         <li class="treeview">
           <a href="#">
             <i class="fa fa-lightbulb-o"></i>
@@ -42,10 +33,6 @@
         </li>
         </li>
         <li><a href="#"><i class="fa fa-television"></i> <span>Television</span></a></li>
-        <li class="header">LABELS</li>
-        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
       </ul>
     </section>
     <!-- /.sidebar -->
