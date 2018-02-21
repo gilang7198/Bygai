eaa559a6c49de89ba008406619027326956b528d
agungpradanaw <33536926+agungpradanaw@users.noreply.github.com>
2018-02-21 14:18:34 +0700

    Userdashboard

--- application/controllers/Userdashboard.php
+++ application/controllers/Userdashboard.php
@@ -6,6 +6,7 @@ class Userdashboard extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('userdashboard/header');
+		$this->load->view('userdashboard/sidebar');
 		$this->load->view('userdashboard/dashboard');
 		$this->load->view('userdashboard/footer');
 	}
---
+++ application/views/userdashboard/sidebar.php
@@ -0,0 +1,53 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li><a href="#"><i data-feather="home"></i> <span>Dashboard</span></a></li>
+        <li class="header">MAIN NAVIGATION</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> <span>Device Option</span></a></li>
+        <li>
+          <a href="pages/widgets.html">
+            <i class="fa fa-th"></i> <span>Widgets</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">new</small>
+            </span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-lightbulb-o"></i>
+            <span>Lampu</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Lampu Tengah</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Lampu Kamar Mandi</a></li></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-snowflake-o"></i> <span>Air Conditioner</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Ruang Tengah Lt. Bawah</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Ruang Tengah Lt. Atas</a></li>
+          </ul>
+        </li>
+        </li>
+        <li><a href="#"><i class="fa fa-television"></i> <span>Television</span></a></li>
+        <li class="header">LABELS</li>
+        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+  <!-- Left side column. contains the logo and sidebar -->
